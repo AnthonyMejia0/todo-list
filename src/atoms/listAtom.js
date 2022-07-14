f6c54d97d286b4d0e5f22bdd0635b1b16527c6b2
AnthonyMejia0 <mejia.anthony96@gmail.com>
2022-07-14 00:39:39 -0700

    added persistent storage

--- src/atoms/listAtom.js
+++ src/atoms/listAtom.js
@@ -1,6 +1,13 @@
 import { atom } from "recoil";
+import { recoilPersist } from "recoil-persist";
+
+const { persistAtom } = recoilPersist({
+    key: 'recoil-persist',
+    storage: localStorage,
+});
 
 export const listState = atom({
     key: 'listState',
     default: [],
+    effects_UNSTABLE: [persistAtom],
 });
